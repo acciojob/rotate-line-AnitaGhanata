c9dbb662101aecc0be975972bd98914f8f08a7e9
Anita <109730681+AnitaGhanata@users.noreply.github.com>
2023-07-27 22:46:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function highlight() {
+    //Write your code here
+  var boldWords = document.querySelectorAll('strong')
+  boldWords.forEach(function(word) {
+    word.style.color = 'green';
+  });
+}
+function return_normal() {
+   var boldWords = document.querySelectorAll('strong')
+  boldWords.forEach(function(word) {
+    word.style.color = 'black';
+  });
+}
